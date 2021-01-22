f374d38dc6acb016ed54db1c4a40edcfdcfa1e19
chocom01 <mukola0101@gmail.com>
2021-01-22 18:44:57 +0200

    add assotiation for item

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,9 +4,11 @@ class Item < ApplicationRecord
   paginates_per 3
 
   belongs_to :user
+  belongs_to :category
 
   has_many :bookings
   has_many :reviews, as: :reviewable
+  has_and_belongs_to_many :options
 
   validates :name, length: { in: 3..10 }
   validates :description, presence: true
--- db/migrate/20210105135300_create_items.rb
+++ db/migrate/20210105135300_create_items.rb
@@ -4,6 +4,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
       t.belongs_to :user
+      t.belongs_to :category
       t.string :name
       t.text :description
       t.float :price
