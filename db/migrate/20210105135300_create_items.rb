dcf8b7ce174121b6ddfc18da398bf497901ace47
chocom01 <mukola0101@gmai.com>
2021-01-23 18:26:57 +0200

    delete excessive assotiation

--- db/migrate/20210105135300_create_items.rb
+++ db/migrate/20210105135300_create_items.rb
@@ -4,7 +4,6 @@ class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
       t.belongs_to :user
-      t.belongs_to :category
       t.string :name
       t.text :description
       t.float :price
