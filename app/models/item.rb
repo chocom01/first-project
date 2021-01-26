a5d51e2da7a4b57fdfc20fa248fb1883d3ee7e82
chocom01 <mukola0101@gmail.com>
2021-01-26 18:47:30 +0200

    edit Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Item < ApplicationRecord
-  paginates_per 3
+  paginates_per 10
 
   belongs_to :user
   belongs_to :category
@@ -16,11 +16,14 @@ class Item < ApplicationRecord
 
   scope :by_city,
         ->(city) { joins(user: :city).where(cities: { id: city }) }
-  scope :filter_by_start_with, ->(name) { where('name like ?', "#{name}%") }
+  scope :by_name, ->(name) { where('name like ?', "#{name}%") }
+  scope :by_user, ->(user_id) { where(user_id: user_id) }
   scope :by_category, ->(category_id) { where(category_id: category_id) }
-  scope :by_options, lambda { |options|
-    joins(:options).where(options: { id: options.pluck(:id) })
-                   .having("count(option_id) = #{options.size}")
+  scope :by_options, lambda { |option_ids|
+    joins(:options).where(options: { id: option_ids })
+                   .having("count(option_id) = #{option_ids.size}")
                    .group(:id)
   }
+  scope :price_min, ->(number) { where(arel_table[:price].gt(number)) }
+  scope :price_max, ->(number) { where(arel_table[:price].lt(number)) }
 end
